a5c0f83a4b87a999b3d39b8437712523172c075d
Prashant Kumar Jha <pkumarjha2310@gmail.com>
2025-03-28 12:37:07 +0530

    Added the CRUD operations for the entities

--- datahandler.py
+++ datahandler.py
@@ -1,5 +1,11 @@
+import json
+import os
 import uuid
-
+from minio import Minio
+import dotenv
+import pandas as pd
+from pandas import DataFrame as df
+from dotenv import dotenv_values
 from psycopg2 import connect
 
 # create connections to database
@@ -7,7 +13,7 @@ from psycopg2 import connect
 # create cursor
 # create table schema
 # execute the connections in db
-
+dotenv.load_dotenv()
 db_config = {
     "dbname": "bms",
     "host": "localhost",
@@ -18,6 +24,8 @@ db_config = {
 
 
 def init_db():
+    # create table if not exist
+
     book_table = "create table book(id UUID PRIMARY KEY,title varchar(50),author varchar(50),genre varchar(30), year_published int,summary varchar(5000))"
     user_table = "create table users(id UUID primary key, name varchar(50))"
     review_table = "create table reviews(id UUID PRIMARY KEY, book_id UUID, user_id UUID, review text, rating decimal, constraint fk_user FOREIGN KEY(user_id) references users(id) on delete cascade, constraint fk_book FOREIGN KEY(book_id) references book(id) on delete cascade )"
@@ -55,3 +63,83 @@ def getUser(user_id:uuid.UUID):
     return user
 
 
+
+def createBook(book):
+    try:
+        if book.id is None:
+            book.id = uuid.uuid4()
+
+        book_dict = book.dict()
+        conn = connect(**db_config)
+        cursor = conn.cursor()
+        cursor.execute("insert into book(id,title,author,genre,year_published,summary) values(%s,%s,%s,%s,%s,%s)", (str(book_dict["id"]), book_dict["title"], book_dict["author"], book_dict["genre"], book_dict["year_published"], ""))
+        updateSummary(book_dict["name"])
+        conn.commit()
+        return book_dict
+
+    except Exception as e:
+        print(e)
+        return None
+async def updateSummary(book_dict):
+    # call LLM to generate summary
+    book_name = book_dict['name']
+    genre = book_dict['genre']
+    base_url = os.getenv("storage_url")
+    file_name = base_url + "/books/"+ genre+"/"+ book_name + ".pdf"
+    minio_client = Minio(os.getenv("minio_url"), access_key=os.getenv("minio_access_key"), secret_key=os.getenv("minio_secret_key"), secure=False)
+    file = minio_client.get_object("books/"+genre, file_name)
+    content = file.read().decode('utf-8')
+    return "summary of the book"
+def getBook(book_id:uuid.UUID):
+    conn = connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("select * from book where id = %s", (str(book_id),))
+    book = cursor.fetchone()
+    return book
+
+def getBook():
+    conn = connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("select * from book")
+    books = cursor.fetchall()
+    return books
+
+def createReview(review):
+    try:
+        if review.id is None:
+            review.id = uuid.uuid4()
+
+        review_dict = review.dict()
+        conn = connect(**db_config)
+        cursor = conn.cursor()
+        cursor.execute("insert into reviews(id,book_id,user_id,review,rating) values(%s,%s,%s,%s,%s)", (str(review_dict["id"]), str(review_dict["book_id"]), str(review_dict["user_id"]), review_dict["review"], review_dict["rating"]))
+        conn.commit()
+        return review_dict
+
+    except Exception as e:
+        print(e)
+        return None
+
+def getReviews(book_id:uuid.UUID):
+    conn = connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("select review,rating from reviews where book_id = %s", (str(book_id),))
+    reviews = cursor.fetchall()
+    summary = summarizeReviews(reviews)
+    return summary
+
+def summarizeReviews(reviews):
+    summary ={}
+    df_reviews = df(reviews, columns=["review","rating"])
+    reviews_list = df_reviews.get("review")
+    summarized_reviews = summarize(reviews_list)
+    summary['summary']= summarized_reviews
+
+    rating_avg = df_reviews.get("rating").avg()
+    summary['rating'] = rating_avg
+    # summary = df_reviews.describe()
+    return summary
+
+def summarize(reviews):
+    # call LLM to summarize the reviews
+    return " ".join(reviews)
--- main.py
+++ main.py
@@ -13,6 +13,21 @@ class User(BaseModel):
     id: Optional[uuid.UUID] = uuid.uuid4()
     name: str = Field(..., example="John Doe")
 
+class Book(BaseModel):
+    id : Optional[uuid.UUID] = uuid.uuid4()
+    title: str = Field(..., example="The Alchemist")
+    author: str = Field(..., example="Paulo Coelho")
+    genre: str = Field(..., example="Adventure")
+    year_published: int = Field(..., example=1988)
+    summary: Optional[str] = Field(default="",example="summary of the book")
+
+class Review(BaseModel):
+    id: Optional[uuid.UUID] = uuid.uuid4()
+    book_id: uuid.UUID
+    user_id: uuid.UUID
+    review: str = Field(..., example="This book is amazing")
+    rating: float = Field(..., example=4.5)
+
 
 @app.get("/user/{user_id}")
 def getUser(user_id: uuid.UUID):
@@ -25,3 +40,13 @@ def createUser(user: User):
     userRes = datahandler.createUser(user)
     return userRes
 
+
+@app.on_event("startup")
+def init_db():
+    print("init db")
+    # datahandler.init_db()
+
+@app.post("/book")
+def createBook(book: Book):
+    bookRes = datahandler.createBook(book)
+    return bookRes
