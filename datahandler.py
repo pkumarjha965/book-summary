2309a3b9bd35a123e9ebc8d8818c15f7d9a9146e
Prashant Kumar Jha <pkumarjha2310@gmail.com>
2025-04-13 14:12:00 +0530

    updated the generate summary code and review response structure

--- datahandler.py
+++ datahandler.py
@@ -1,6 +1,8 @@
 import json
 import os
 import uuid
+
+from fastapi import requests
 from minio import Minio
 import dotenv
 import pandas as pd
@@ -46,7 +48,7 @@ def createUser(user):
         print(user_dict)
         conn = connect(**db_config)
         cursor = conn.cursor()
-        cursor.execute("insert into users(id,name) values(%s,%s)", (str (user_dict["id"]), user_dict["name"]))
+        cursor.execute("insert into users(id,name) values(%s,%s)", (str(user_dict["id"]), user_dict["name"]))
         conn.commit()
         return user_dict
 
@@ -55,13 +57,12 @@ def createUser(user):
         return None
 
 
-def getUser(user_id:uuid.UUID):
+def getUser(user_id: uuid.UUID):
     conn = connect(**db_config)
     cursor = conn.cursor()
     cursor.execute("select * from users where id = %s", (str(user_id),))
     user = cursor.fetchone()
     return user
-
 
 
 def createBook(book):
@@ -72,37 +73,78 @@ def createBook(book):
         book_dict = book.dict()
         conn = connect(**db_config)
         cursor = conn.cursor()
-        cursor.execute("insert into book(id,title,author,genre,year_published,summary) values(%s,%s,%s,%s,%s,%s)", (str(book_dict["id"]), book_dict["title"], book_dict["author"], book_dict["genre"], book_dict["year_published"], ""))
-        # updateSummary(book_dict["name"])
+        cursor.execute("insert into book(id,title,author,genre,year_published,summary) values(%s,%s,%s,%s,%s,%s)", (
+            str(book_dict["id"]), book_dict["title"], book_dict["author"], book_dict["genre"],
+            book_dict["year_published"],
+            ""))
         conn.commit()
         conn.close()
-        return book_dict
 
+        updateSummary(book_dict["id"], book_dict["name"])
+        return book_dict
 
     except Exception as e:
         print(e)
         return None
-async def updateSummary(book_dict):
+
+
+async def getBookContent(book_dict):
     # call LLM to generate summary
     book_name = book_dict['name']
     genre = book_dict['genre']
     base_url = os.getenv("storage_url")
-    file_name = base_url + "/books/"+ genre+"/"+ book_name + ".pdf"
-    minio_client = Minio(os.getenv("minio_url"), access_key=os.getenv("minio_access_key"), secret_key=os.getenv("minio_secret_key"), secure=False)
-    file = minio_client.get_object("books/"+genre, file_name)
+    file_name = base_url + "/books/" + genre + "/" + book_name + ".pdf"
+    minio_client = Minio(os.getenv("minio_url"), access_key=os.getenv("minio_access_key"),
+                         secret_key=os.getenv("minio_secret_key"), secure=False)
+    file = minio_client.get_object("books/" + genre, file_name)
     content = file.read().decode('utf-8')
-    summary = generateSummary(content) # call LLM to generate summary
-    return summary
+    return content
 
-def generateSummary(content):
+
+async def updateSummary(id, content):
     # call LLM to generate summary
-    return "summary of the book"
-def getBook(book_id:uuid.UUID):
+
+    summary = await generateSummary(content)  # call LLM to generate summary
+    conn = connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("update book set summary = %s where id = %s", (summary, id))
+    conn.commit()
+    conn.close()
+
+
+async def generateSummary(content):
+    # call LLM to generate summary
+    url = os.getenv("llm_url")
+    headers = {
+        "content-type": "application/json",
+        "accept": "application/json"
+    }
+
+    requestObj = {
+        "model": "llama3.2",
+        "message": [{"role": "system",
+                     "content": "You are an assistant, who reads the content of a book and generate the summary of the book."},
+                    {"role": "user", "content": content}],
+        "stream": False
+    }
+
+    response = requests.post(url, headers=headers, json=requestObj)
+
+    if (response.status_code != 200):
+        print("Error in generating summary")
+        return None
+    response = response.json()
+    summary_content = response.get("message").get("content")
+    return summary_content
+
+
+def getBook(book_id: uuid.UUID):
     conn = connect(**db_config)
     cursor = conn.cursor()
     cursor.execute("select * from book where id = %s", (str(book_id),))
     book = cursor.fetchone()
     return book
+
 
 def getBook():
     conn = connect(**db_config)
@@ -111,7 +153,42 @@ def getBook():
     books = cursor.fetchall()
     return books
 
-def createReview(review):
+
+def getBook(book_id: uuid.UUID):
+    conn = connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("select * from book where id = %s", (str(book_id),))
+    books = cursor.fetchone()
+    return books
+
+def deleteBook(book_id: uuid.UUID):
+    try:
+        conn = connect(**db_config)
+        cursor = conn.cursor()
+        cursor.execute("delete from book where id = %s", (str(book_id),))
+        conn.commit()
+        return book_id
+
+    except Exception as e:
+        print(e)
+        return None
+def updateBook(book):
+    try:
+        book_dict = book.dict()
+        conn = connect(**db_config)
+        cursor = conn.cursor()
+        cursor.execute("update book set title = %s, author = %s, genre = %s, year_published = %s where id = %s",
+                       (book_dict["title"], book_dict["author"], book_dict["genre"], book_dict["year_published"],
+                        str(book_dict["id"])))
+        conn.commit()
+        return book_dict
+
+    except Exception as e:
+        print(e)
+        return None
+
+
+def createReview(book_id,review):
     try:
         if review.id is None:
             review.id = uuid.uuid4()
@@ -119,7 +196,9 @@ def createReview(review):
         review_dict = review.dict()
         conn = connect(**db_config)
         cursor = conn.cursor()
-        cursor.execute("insert into reviews(id,book_id,user_id,review,rating) values(%s,%s,%s,%s,%s)", (str(review_dict["id"]), str(review_dict["book_id"]), str(review_dict["user_id"]), review_dict["review"], review_dict["rating"]))
+        cursor.execute("insert into reviews(id,book_id,user_id,review,rating) values(%s,%s,%s,%s,%s)", (
+            str(review_dict["id"]), str(book_id), str(review_dict["user_id"]), review_dict["review"],
+            review_dict["rating"]))
         conn.commit()
         return review_dict
 
@@ -127,26 +206,47 @@ def createReview(review):
         print(e)
         return None
 
-def getReviews(book_id:uuid.UUID):
+
+def getReviews(book_id: uuid.UUID):
     conn = connect(**db_config)
     cursor = conn.cursor()
-    cursor.execute("select review,rating from reviews where book_id = %s", (str(book_id),))
+    cursor.execute("select * from reviews where book_id = %s", (str(book_id),))
     reviews = cursor.fetchall()
-    summary = summarizeReviews(reviews)
-    return summary
+    # summary = summarizeReviews(reviews)
+    # convert revies to dictionary
+    reviews_list = []
+
+    return reviews
+
 
 def summarizeReviews(reviews):
-    summary ={}
-    df_reviews = df(reviews, columns=["review","rating"])
+    summary = {}
+    df_reviews = df(reviews, columns=["review", "rating"])
     reviews_list = df_reviews.get("review")
     summarized_reviews = summarize(reviews_list)
-    summary['summary']= summarized_reviews
+    summary['summary'] = summarized_reviews
 
-    rating_avg = df_reviews.get("rating").avg()
+    rating_avg = df_reviews.get("rating")
+    rating_avg = rating_avg.mean()
     summary['rating'] = rating_avg
     # summary = df_reviews.describe()
     return summary
 
+
 def summarize(reviews):
     # call LLM to summarize the reviews
     return " ".join(reviews)
+
+def getBookSummary(book_id: uuid.UUID):
+    conn = connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("select summary from book where id = %s", (str(book_id),))
+    book_details = cursor.fetchone()
+    if(book_details is None):
+        return None
+    summary = book_details[5]
+    if(summary is None):
+        content = book_details[4]
+        summary = generateSummary(content)
+
+    return summary
--- main.py
+++ main.py
@@ -9,17 +9,26 @@ import datahandler
 
 app = FastAPI()
 
+
 class User(BaseModel):
     id: Optional[uuid.UUID] = uuid.uuid4()
     name: str = Field(..., example="John Doe")
 
+
+class responseModel(BaseModel):
+    id: uuid.UUID = uuid.uuid4()
+    message: str = Field(..., example="User created successfully")
+    status: bool = Field(..., example=True)
+
+
 class Book(BaseModel):
-    id : Optional[uuid.UUID] = uuid.uuid4()
+    id: Optional[uuid.UUID] = uuid.uuid4()
     title: str = Field(..., example="The Alchemist")
     author: str = Field(..., example="Paulo Coelho")
     genre: str = Field(..., example="Adventure")
     year_published: int = Field(..., example=1988)
-    summary: Optional[str] = Field(default="",example="summary of the book")
+    summary: Optional[str] = Field(default="", example="summary of the book")
+
 
 class Review(BaseModel):
     id: Optional[uuid.UUID] = uuid.uuid4()
@@ -46,7 +55,52 @@ def init_db():
     print("init db")
     # datahandler.init_db()
 
+
 @app.post("/book")
 def createBook(book: Book):
     bookRes = datahandler.createBook(book)
     return bookRes
+
+
+@app.put("/book")
+def updateBook(book: Book):
+    bookRes = datahandler.updateBook(book)
+    return bookRes
+
+
+@app.delete("/book/{book_id}")
+def deleteBook(book_id: uuid.UUID):
+    book = datahandler.deleteBook(book_id)
+    return responseModel(book.id, "Book deleted successfully", True)
+
+
+@app.get("/book/{book_id}", response_model=Book, tags=["book"])
+def getBook(book_id: uuid.UUID):
+    book = datahandler.getBook(book_id)
+    # convert to Book model
+    book = Book(id=book[0], title=book[1], author=book[2], genre=book[3], year_published=book[4], summary=book[5])
+    return book
+
+@app.get("/book/{book_id}/review")
+def getReview(book_id: uuid.UUID):
+    reviews = datahandler.getReviews(book_id)
+    # convert to Review model
+    reviewModels = []
+    review = reviews[0]
+    review_model = Review(id=review[0], book_id=review[1], user_id=review[2], review=review[3], rating=review[4])
+    for review in reviews:
+        reviewModels.append(
+            Review(id=review[0], book_id=review[1], user_id=review[2], review=review[3], rating=review[4]))
+    print(reviewModels)
+    return reviewModels
+
+@app.post("/book/{id}/review")
+def createReview(id:uuid.UUID, review: Review):
+    reviewRes = datahandler.createReview(review)
+    return responseModel(reviewRes.id, "Review created successfully", True)
+
+
+@app.get("/book/{book_id}/summary")
+def getBookSummary(book_id: uuid.UUID):
+    summary = datahandler.getBookSummary(book_id)
+    return summary
